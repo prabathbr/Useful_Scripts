07b105ec82e05e3356339b80b05f05318b218b52
prabathbr <60564552+prabathbr@users.noreply.github.com>
2023-10-06 16:38:56 +1100

    Create UnMapDrives.cmd
    Unmap all network drives

---
+++ UnMapDrives.cmd
@@ -0,0 +1,15 @@
+C:\Windows\system32>NET USE * /d /y
+You have these remote connections:
+
+    V:              \\nas\NAS_2TB
+    W:              \\M700\d
+    X:              \\Nas\nas_sync
+    Y:              \\Nas\nas_8tb
+    Z:              \\Nas\nas_4tb
+Continuing will cancel the connections.
+
+The command completed successfully.
+
+
+C:\Windows\system32>
+
